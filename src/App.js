a96c1f39e2050cb4dd06321174f59ac3e8a333e5
Nikita Dhingra <developer.nikita11@gmail.com>
2024-11-25 15:24:46 +0530

    privacy page added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}></Route>
           <Route path="/home" element={<Home/>}></Route>
+          <Route path="/privacy" element={<PrivacyPolicy />}></Route>
         </Routes>
       </Router>
       
